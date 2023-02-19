5b2dc95c4d0b98095a5b88913dc56bfa676f7f48
mohanadft <mohanadfteha@gmail.com>
2023-02-20 01:41:16 +0200

    Clear the data before each test

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -2,35 +2,66 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from '../src/app.module';
+import { PrismaService } from '../src/modules/prisma/prisma.service';
 
 describe('Authentication System', () => {
   let app: INestApplication;
+  let prismaService: PrismaService;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
+      providers: [
+        {
+          provide: PrismaService,
+          useValue: prismaService,
+        },
+      ],
     }).compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
+
+    prismaService = app.get(PrismaService);
+    await prismaService.user.deleteMany();
   });
 
   it('can user signup', async () => {
     const user = {
-      email: '1aasdasdsd@gmail.com',
+      email: '2@gmail.com',
       password: 'asd',
     };
 
-    const {
-      body: { data },
-    } = await request(app.getHttpServer())
+    const { body } = await request(app.getHttpServer())
       .post('/auth/signup')
       .send({
         ...user,
       })
       .expect(201);
 
-    expect(data.email).toEqual(user.email);
-    expect(data.password).not.toEqual(user.password);
+    expect(body.data.email).toEqual(user.email);
+    expect(body.data.password).not.toEqual(user.password);
+
+    expect(body.access_token).toBeDefined();
+  });
+
+  it('can the signed in user access routes', async () => {
+    const user = {
+      email: '2@gmail.com',
+      password: 'asd',
+    };
+
+    const { body } = await request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({ ...user })
+      .expect(201);
+
+    const { access_token } = body;
+
+    return await request(app.getHttpServer())
+      .get('/tasks')
+      .set('Authorization', `Bearer ${access_token}`)
+      .send({ ...user })
+      .expect(200);
   });
 });

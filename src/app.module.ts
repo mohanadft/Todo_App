2ad469154ca253c50a12a1d9ff18edc418b97334
mohanadft <mohanadfteha@gmail.com>
2023-02-20 01:15:29 +0200

    Change config file
    Making a condition to the file path, so that when entering testing environment the environment variables will be loaded from a different file than the one with development

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,7 @@ import { APP_PIPE } from '@nestjs/core';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      file: '.env',
-    }),
+    ConfigModule.forRoot({ file: `.env.${process.env.NODE_ENV}` }),
     UserModule,
     TaskModule,
     AuthModule,

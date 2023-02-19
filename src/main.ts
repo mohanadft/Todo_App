b752591fe8ac7732229f793d5e767fb9c27e95c9
mohanadft <mohanadfteha@gmail.com>
2023-02-20 01:41:56 +0200

    Modify port
    Get the port from config service

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from './modules/config/config.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  await app.listen(process.env.PORT);
+  const configService = app.get(ConfigService);
+  await app.listen(configService.get('PORT'));
 }
 
 bootstrap();

bb9a3947acc0beed9c043561560577c67704dcf3
mohanadft <mohanadfteha@gmail.com>
2023-04-20 21:38:29 +0200

    asd

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,12 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { ConfigService } from './modules/config/config.service';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { ConfigService } from "./modules/config/config.service";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
 
-
-  const configService = app.get(ConfigService); 
-  await app.listen(configService.get('PORT'));
+  const configService = app.get(ConfigService);
+  await app.listen(configService.get("PORT"));
 }
 
 bootstrap();
